ac2903825695d4d56c65352f9a939b5330933bdd
Adam Folke <Adam.Folke@hudiksvall.se>
2024-02-04 12:22:06 +0100

    Add project files.

---
+++ MVVMExempel/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMExempel.Model
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
